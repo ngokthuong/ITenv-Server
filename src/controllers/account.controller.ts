5e2d421fd142762b04fd01161db5712cbcfc4713
Ngọc Thương <waytnt17523@gmail.com>
2024-09-11 10:56:55 +0700

    auth Final

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,12 +1,11 @@
-import { response } from "express";
-import asyncHandeler from "express-async-handler";
-import { registerService, loginService } from '../services/index.services';
+import asyncHandler from "express-async-handler";
+import { registerService, loginService, exchangeGithubCodeForToken, fetchGithubUserData, fetchGithubUserEmail } from '../services/index.services';
 import { Request, Response } from 'express';
 import schema from "../helper/joiSchema.helper";
 import axios from "axios";
 // use express-async-handler
 
-export const registerController = asyncHandeler(async (req: any, res: any) => {
+export const registerController = asyncHandler(async (req: any, res: any) => {
     const { error } = schema.validate(req.body, { allowUnknown: true });
     if (error) {
         return res.status(400).json({
@@ -28,18 +27,15 @@ export const registerController = asyncHandeler(async (req: any, res: any) => {
     }
 });
 
-
-export const loginController = asyncHandeler(async (req: any, res: any) => {
+export const loginController = asyncHandler(async (req: any, res: any) => {
     try {
-        console.log('loginController body')
-        console.log(req.body)
-        const { accessToken, refreshToken, accountData } = await loginService(req.body);
+        const { accessToken, refreshToken, dataResponse } = await loginService(req.body);
         // save refreshToken in cookie 
         res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 1000 })
         return res.status(200).json({
             success: true,
             accessToken,
-            accountData
+            dataResponse
         });
     } catch (error: any) {
         return res.status(401).json({
@@ -51,55 +47,24 @@ export const loginController = asyncHandeler(async (req: any, res: any) => {
 
 
 
-export const githubOauthController = asyncHandeler(async (req: Request, res: Response, next) => {
-    console.log('body github')
+
+export const githubOauthController = asyncHandler(async (req: Request, res: Response, next) => {
     const { code } = req.body;
-    console.log(code);
-
     try {
-        // Exchange the code for an access token
-        const tokenResponse = await axios.post(
-            'https://github.com/login/oauth/access_token',
-            {
-                client_id: process.env.GITHUB_OAUTH_CLIENT_ID,
-                client_secret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
-                code,
-            },
-            { headers: { Accept: 'application/json' } },
-        );
-        const { access_token } = tokenResponse.data;
-        // Fetch user data
-        const userResponse = await fetch('https://api.github.com/user', {
-            headers: {
-                Authorization: `Bearer ${access_token}`,
-            },
-        });
-        const userData = await userResponse.json();
+        const accessToken = await exchangeGithubCodeForToken(code);
+        const userData = await fetchGithubUserData(accessToken);
+        const email = await fetchGithubUserEmail(accessToken);
+        userData.email = email || userData.email;
 
-        const emailResponse = await fetch('https://api.github.com/user/emails', {
-            headers: {
-                Authorization: `Bearer ${access_token}`,
-            },
-        });
-        const emailData = await emailResponse.json();
-        console.log(emailData[0]);
-        if (emailData[0].email) {
-            userData.email = emailData[0].email;
-        }
-        userData.firstName = userData.login;
-        userData.lastName = userData.login;
-        userData.authenWith = 3;
-        const account = {
+        const dataResp = {
             email: userData.email,
-            firstName: userData.firstName,
-            lastName: userData.lastName,
-            authenWith: userData.authenWith,
+            username: userData.name,
+            authenWith: 3,
         };
-        req.body = account;
+
+        req.body = dataResp;
         loginController(req, res, next);
-        // res.status(200).json({ success: true, user: userData });
     } catch (error) {
-        console.error('Error exchanging token or fetching user data:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
--- src/services/account.services.ts
+++ src/services/account.services.ts
@@ -1,11 +1,12 @@
 import Account from "../model/account";
 import User from "../model/user";
 import { generateAccessToken, generateRefreshToken } from '../middleware/jwt.mdw'
+import lodash from 'lodash'
+import axios from "axios";
 
 const registerService = async (body: any) => {
     const { email } = body;
     const account = await Account.findOne({ email });
-
     if (account) {
         throw new Error('The email has already been used.');
     } else {
@@ -29,31 +30,26 @@ const registerAccount = async (data: any) => {
         account: [account._id]
     })
     account.user = user._id
-    await account.save();
-    return account
+    return await account.save();
 }
 
 const createAccountWithOldUserID = (data: any) => {
     return new Promise(async (resolve, reject) => {
         try {
-            // check account existed 
             const accountExist = await Account.findOne({ email: data.email });
             if (accountExist) {
                 // Assign the UserID from the existing account to the data
                 data.user = accountExist.user;
+                const newAccount = await Account.create(data);
+                // find the corresponding user
+                const userExist = await User.findOne({ _id: newAccount.user });
+                if (userExist) {
+                    userExist.account.push(newAccount._id);
+                    await userExist.save();
+                }
+                resolve(newAccount);
             }
-            // create new account 
-            const newAccount = await Account.create(data);
-            // find the corresponding user
-            const userExist = await User.findOne({ _id: newAccount.user });
-            if (userExist) {
-                // add account ID in account aray of user
-                userExist.account.push(newAccount._id);
-                await userExist.save();
-            }
-            resolve(newAccount);
         } catch (error: any) {
-            // if error, reject with a error memo
             reject(new Error(`Failed to create new account with UserId: ${error.message}`));
         }
     });
@@ -68,35 +64,78 @@ const createAllToken = async (account: any) => {
     await User.findByIdAndUpdate(account.user, { refreshToken }, { new: true })
     return { accessToken, refreshToken }
 }
+
+const dataResponseClient = async (account: any, user: any) => {
+    const accountData = await lodash.omit(account.toObject(), ['_id', 'role', 'password', 'passwordChangeAt', 'passwordResetToken', 'passwordResetExpires', 'user'])
+    const { accessToken, refreshToken } = await createAllToken(account);
+    const dataResponse = await { accountData, userName: user?.username || "no data" }
+    return { dataResponse, accessToken, refreshToken }
+}
+
 const loginService = async (data: any) => {
     const { email, authenWith, password } = data;
-    // Tìm tài khoản với email
+    // find account with email
     const account = await Account.findOne({ email });
-    // Xử lý khi `authenWith === 0` và so sánh mật khẩu
+    console.log(account)
+    const user = await User.findById(account?.user);
+    console.log(user)
+    // if else with account.authenWith ==0 and compare password
     if (account && authenWith === 0 && account.authenWith === 0) {
-        if (password === account.password) {
-            const { role, ...accountData } = account.toObject();
-            const { accessToken, refreshToken } = await createAllToken(account);
-            return { accessToken, refreshToken, accountData };
+        if (await account.isCorrectPassword(password)) {
+            const { dataResponse, accessToken, refreshToken } = await dataResponseClient(account, user);
+
+            return { accessToken, refreshToken, dataResponse };
         }
         throw new Error('Invalid credentials!');
     }
-    // Xử lý khi `authenWith` thuộc giá trị 1, 2, hoặc 3
+    // if authen is [1||2||3] 
     if (authenWith >= 1 && authenWith <= 3) {
         const existingAccounts = await Account.find({ email, authenWith: { $in: [1, 2, 3] } });
         const existingAuthenWith = existingAccounts.map(acc => acc.authenWith);
-        // Nếu không có tài khoản với `authenWith` hiện tại, tạo tài khoản mới
+        // If there isn't an account with the specified authenWith, then create a new account with a new authenWith
         if (!existingAuthenWith.includes(authenWith)) {
             const newAccount = account ? await createAccountWithOldUserID(data) : await registerAccount(data);
-            const { accessToken, refreshToken } = await createAllToken(newAccount);
-            return { accessToken, refreshToken, accountData: newAccount };
+            const { dataResponse, accessToken, refreshToken } = await dataResponseClient(newAccount, user);
+            return { accessToken, refreshToken, dataResponse };
         }
-        // Nếu đã tồn tại tài khoản với `authenWith`, trả về token
+        // if account with authenWith is existed then return token and accountWithMailAuth
         const accountWithMailAuth = await Account.findOne({ email, authenWith });
-        const { accessToken, refreshToken } = await createAllToken(accountWithMailAuth);
-        return { accessToken, refreshToken, accountData: accountWithMailAuth }
+        const { dataResponse, accessToken, refreshToken } = await dataResponseClient(accountWithMailAuth, user);
+        return { accessToken, refreshToken, dataResponse }
     }
     throw new Error('Invalid credentials!');
 };
 
-export { registerService, loginService }
+const exchangeGithubCodeForToken = async (code: string) => {
+    const tokenResponse = await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+            client_id: process.env.GITHUB_OAUTH_CLIENT_ID,
+            client_secret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
+            code,
+        },
+        { headers: { Accept: 'application/json' } }
+    );
+    return tokenResponse.data.access_token;
+};
+
+const fetchGithubUserData = async (accessToken: string) => {
+    const userResponse = await fetch('https://api.github.com/user', {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return userResponse.json();
+};
+
+const fetchGithubUserEmail = async (accessToken: string) => {
+    const emailResponse = await fetch('https://api.github.com/user/emails', {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    const emailData = await emailResponse.json();
+    return emailData[0]?.email;
+};
+
+export { registerService, loginService, exchangeGithubCodeForToken, fetchGithubUserData, fetchGithubUserEmail }
