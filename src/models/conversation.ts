d38ff611a3c09fb894fcdab68cb6ba6be810e53b
tranduongthieu <thieu098dm3@gmail.com>
2024-11-08 16:43:32 +0700

    hotifx: fix createdBy model

--- src/models/conversation.ts
+++ src/models/conversation.ts
@@ -1,44 +1,46 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IConversation extends Document {
-    _id: mongoose.Types.ObjectId;
-    createdBy: mongoose.Types.ObjectId;
-    participants: mongoose.Types.ObjectId[];
-    isGroupChat?: boolean;
-    groupName?: string;
-    isDeleted?: boolean
-    lastMessage?: mongoose.Types.ObjectId;
+  _id: mongoose.Types.ObjectId;
+  createdBy: mongoose.Types.ObjectId;
+  participants: mongoose.Types.ObjectId[];
+  isGroupChat?: boolean;
+  groupName?: string;
+  isDeleted?: boolean;
+  lastMessage?: mongoose.Types.ObjectId;
 }
 
 const conversationSchema: Schema<IConversation> = new mongoose.Schema(
-    {
-        createdBy: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        }],
-        participants: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        }],
-        isGroupChat: {
-            type: Boolean,
-            default: false,
-        },
-        groupName: {
-            type: String
-        },
-        isDeleted: {
-            type: Boolean,
-            default: false
-        },
-        lastMessage: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Message"
-        }
+  {
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
-    { timestamps: true }
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    ],
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    groupName: {
+      type: String,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message',
+    },
+  },
+  { timestamps: true },
 );
 
 // conversationSchema.path('participants').validate(function (value) {
